647fcaba2eec86a08dce3222e21330ae600e8848
hemansin <hemansin>
2017-08-01 17:08:49 +0530

    angular 4 with routing config

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,11 +1,3 @@
-# Angular 4 Routing
-
-Github - https://github.com/hs950559/ng4-routing
-
-## Configuring Routes
-
-```javascript
-// app/app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -16,7 +8,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'posts', loadChildren: 'app/blog/post/post.module#PostModule' },
+  { path: 'posts', loadChildren: 'app/posts/posts.module#PostsModule' },
   {
     path: '',
     redirectTo: '/home',
@@ -30,18 +22,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-```
-
-
-# Resources
-
-Create Cover
-
-https://www.canva.com/design/DACc3Wg0tvc/2WFWvSS7tYvvnjiVpA9U2w/edit?layouts=&layoutQuery=free&utm_source=onboarding
-
-
-
-
-
-
-
